84fd9d01a612b27bb51ca6e5ab2d9f0f4937569d
Ranchonyx <felixjanetzki@gmail.com>
2021-07-13 02:20:03 +0200

    Extended the rstdlib and made it bootable on actual BIOS devives. shit works on real hardware niw

--- source/bootloader/boot1.asm
+++ source/bootloader/boot1.asm
@@ -5,24 +5,72 @@ bits      16              ;Currently 16-Bit real mode
 ;Jump to bootloader entry point
 start: jmp loader_entry
 ; Define Hard(bass)coded strings
-launch      db "[OnyxOS] Launching Bootloader..."     ,0
-bootmsg_0   db "[OnyxOS] Bootloader Stage 1 Complete" ,0
-bootmsg_1   db "[OnyxOS] Boot Stage 2 Complete"       ,0
-bootmsg_2   db "[OnyxOS] Boot Stage 3 Complete"       ,0
+asciitest     db "[OnyxOs] Printing available ASCII Characters",0
+error         db "[OnyxOS] Error during Boot"                  ,0
+done          db "[OnyxOS] Done"                               ,0
+launch      	db "[OnyxOS] Starting Bootloader"                ,0
+bootmsg_0   	db "[OnyxOS] Bootloader Stage 1 Complete"        ,0
+bootmsg_1   	db "[OnyxOS] Bootloader Stage 2 Complete"        ,0
+bootmsg_2     db "[OnyxOS] Bootloader Stage 3 Complete"        ,0
 
 ;Include the retard standard assembly library
 %include "../lib/rstdlib.asm"
+
+
+;*************************************************;
+;	OEM Parameter block / BIOS Parameter Block
+;*************************************************;
+
+;TIMES 0Bh-$+start DB 0
+
+bpbBytesPerSector:  	DW 512
+bpbSectorsPerCluster: 	DB 1
+bpbReservedSectors: 	DW 1
+bpbNumberOfFATs: 	DB 2
+bpbRootEntries: 	DW 224
+bpbTotalSectors: 	DW 2880
+bpbMedia: 	        DB 0xF0
+bpbSectorsPerFAT: 	DW 9
+bpbSectorsPerTrack: 	DW 18
+bpbHeadsPerCylinder: 	DW 2
+bpbHiddenSectors:       DD 0
+bpbTotalSectorsBig:     DD 0
+bsDriveNumber: 	        DB 0
+bsUnused: 	        DB 0
+bsExtBootSignature: 	DB 0x29
+bsSerialNumber:	        DD 0xa0a1a2a3
+bsVolumeLabel: 	        DB "MOS FLOPPY "
+bsFileSystem: 	        DB "FAT12   "
 
 ;*************************************************;
 ;	Bootloader Entry Point
 ;*************************************************;
 loader_entry:
 
+call Clearscr
+
+mov bl, 1
 mov si, launch
-call Print
+call PrintString
+
+mov si, asciitest
+call PrintString
+
+call PrintNewline
+
+mov bl, 0
+call PrintAsciiCharacters
+
+call PrintNewline
+
+mov bl, 1
+mov si, done
+call PrintString
 
 mov si, bootmsg_0
-call Print
+call PrintString
+
+call PrintMemory
 
 cli                       ;Clear all interrupts
 hlt                       ;Halt the system
--- source/lib/rstdlib.asm
+++ source/lib/rstdlib.asm
@@ -1,18 +1,88 @@
-;***************************************
+;**************************************
+; Prints a newline (0xdh, 0xah)
+;**************************************
+PrintNewline:
+mov al, 0Dh
+mov ah, 0eh
+int 10h
+mov al, 0Ah
+int 10h
+ret
+
+;**************************************
+; Prints a single character/value/register
+; AL: register
+;**************************************
+PrintChar:
+mov ah, 0eh
+int 10h
+cmp bl, 1
+je PrintNewline
+ret
+
+;**************************************
 ;	Prints a string
 ;	DS=>SI: 0 terminated string
-;***************************************
-Print:
-			xor ax,ax
-			mov ds,ax
-			mov es,ax
+;**************************************
+PrintString:
+			xor 			ax,ax
+			mov 			ds,ax
+			mov 			es,ax
 			lodsb					; load next byte from string from SI to AL
-			or			al, al		; Does AL=0?
-			jz			PrintDone	; Yep, null terminator found-bail out
-			mov			ah,	0eh	; Nope-Print the character
-			int			10h
-			jmp			Print		; Repeat until null terminator found
+			or				al, al		; Is AL=0?
+			jz				PrintDone	; Yep, null terminator found-bail out
+			mov				ah,	0eh	; Nope Print the character
+			int				10h
+			jmp				PrintString		; Repeat until null terminator found
 PrintDone:
-			xor ax,ax
-			int 0x12
+			cmp bl, 1
+			je PrintNewline
+			xor 	ax,ax
+			int 	0x12
 			ret					; we are done, so return
+
+;**************************************
+; Print all Ascii Characters
+;**************************************
+PrintAsciiCharacters:
+loop:
+inc al
+cmp al, 255
+je exit
+call PrintChar
+jmp loop
+exit:
+ret
+
+;**************************************
+; Clear the screen
+;**************************************
+Clearscr:
+pusha
+mov ah, 0x00
+mov al, 0x03  ; text mode 80x25 16 colours
+int 0x10
+popa
+ret
+
+;**************************************
+; Print available memory
+;**************************************
+PrintMemory:
+xor cx, cx
+xor dx, dx
+mov ax, 0xe801
+int 0x15
+
+;????? osdev is weird sometimes
+
+mov ax, cx
+mov bx, dx
+
+shr al, 8
+and ah, 0x00ff
+
+call PrintChar
+mov al, ah
+call PrintChar
+ret
