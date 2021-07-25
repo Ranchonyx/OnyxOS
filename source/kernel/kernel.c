5632f032a7fd03c31717120b12b713805acc271d
Ranchonyx <felixjanetzki@gmail.com>
2021-07-25 18:16:39 +0200

    Added a string type under "source/lib/c/types.h"

--- source/kernel/kernel.c
+++ source/kernel/kernel.c
@@ -1,9 +1,11 @@
 #include "../drivers/vga.h"
+#include "../lib/c/types.h"
 
+//Main kernel entry point
 void main() {
 	delaySeconds(2);
 	clrscr(PURPLE_ON_BLACK);
 	println_string("OnyxOS Version 0.0.4");
 	println_string("Copyright (c) 2021 Omer Shamai, Yuri Khordal, Felix Janetzki");
-	
+
 }
