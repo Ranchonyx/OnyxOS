d4115a4d8e2ea2b253af44be12ed12c54b0afa79
Ranchonyx <felixjanetzki@gmail.com>
2021-08-01 21:21:27 +0200

    .

--- source/bootloader/mbr.asm
+++ source/bootloader/mbr.asm
@@ -66,22 +66,7 @@ lkernel:
 
 [bits 32]
 BEGIN_32_BIT:
-; ;Check if CPUID is supported
-; pushfd                               ;Save EFLAGS
-; pushfd                               ;Store EFLAGS
-; xor dword [esp],0x00200000           ;Invert the ID bit in stored EFLAGS
-; popfd                                ;Load stored EFLAGS (with ID bit inverted)
-; pushfd                               ;Store EFLAGS again (ID bit may or may not be inverted)
-; pop eax                              ;eax = modified EFLAGS (ID bit may or may not be inverted)
-; xor eax,[esp]                        ;eax = whichever bits were changed
-; popfd                                ;Restore original EFLAGS
-; and eax,0x00200000                   ;eax = zero if ID bit can't be changed, else non-zero
-;
-; cmp eax, 0
-; jne kstart
-; mov eax, 00h
-; cpuid
-; call print32
+
 
 call KOFFSET
 jmp $
